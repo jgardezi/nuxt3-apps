5c78426838fdaaae62669e1c7fc5f87be4693f7a
Javed Gardezi <javed.gardezi@primexconnect.com>
2022-07-16 15:51:25 +1000

    Added bootstrap, navbar and home page setup

--- restauranty/app.vue
+++ restauranty/app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Resturantly</h1>
+      <a href="/resturants">Go to resturants</a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .container {
+    text-align: center;
+    margin-top: 5rem;
+  }
+</style>
